fa38120c53474909a2b56fd6d95e89bae4632380
hideame <hamezawa3@gmail.com>
2023-01-08 15:06:32 +0900

    add forms.py

--- lunchmap/urls.py
+++ lunchmap/urls.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("main/", include("main.urls")),
     path("admin/", admin.site.urls),
+    path("main/", include("main.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", RedirectView.as_view(url="/main/")),
 ]
